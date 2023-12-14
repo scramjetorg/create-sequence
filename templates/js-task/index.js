96795567c056a552e287dd30795a220de343df48
Budleigh Salterton <cz@scramjet.org>
2023-12-14 13:54:03 +0000

    Fix js example with api call

--- templates/js-task/index.js
+++ templates/js-task/index.js
@@ -2,7 +2,7 @@ async function getPageFromAPI() {
     return "Your result";
 }
 
-module.exports = async function () {
-    await new Promise(res => setTimeout(res, 10000));
+module.exports = async function (_stream, timeout = 10000) {
+    await new Promise(res => setTimeout(res, timeout));
     return getPageFromAPI();
 }
